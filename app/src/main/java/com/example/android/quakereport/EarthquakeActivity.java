4aa9051e4a9191cd07839df6b4dc599cc027713b
João Monteiro <joaoagrom@gmail.com>
2019-03-20 10:39:17 -0300

    handling user connection button to retry request progressbar on pre execute

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,17 +15,23 @@
  */
 package com.example.android.quakereport;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import java.lang.ref.WeakReference;
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 
 public class EarthquakeActivity extends AppCompatActivity {
@@ -34,6 +40,9 @@ public class EarthquakeActivity extends AppCompatActivity {
     private static final String URL_TO_REQUEST = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=6&limit=10";
 
     EarthquakeArrayAdapter mAdapter;
+    ProgressBar mLoadingProgressBar;
+    TextView mInfoText;
+    Button mTryAgainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +50,17 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        mLoadingProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        mInfoText = (TextView) findViewById(R.id.info_text);
+
+        mTryAgainButton = (Button) findViewById(R.id.try_again);
+        mTryAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                performHttpRequest();
+            }
+        });
+
         mAdapter = new EarthquakeArrayAdapter(this, new ArrayList<Earthquake>());
         earthquakeListView.setAdapter(mAdapter);
 
@@ -55,11 +75,36 @@ public class EarthquakeActivity extends AppCompatActivity {
             }
         });
 
-        HttpRequestTask httpRequestTask = new HttpRequestTask();
-        httpRequestTask.execute(URL_TO_REQUEST);
+        performHttpRequest();
     }
 
-    private class HttpRequestTask extends AsyncTask<String, Void, ArrayList<Earthquake>> {
+    private void performHttpRequest() {
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = null;
+        if (connectivityManager != null) {
+            networkInfo = connectivityManager.getActiveNetworkInfo();
+        }
+        if (networkInfo != null && networkInfo.isConnected()) {
+            mInfoText.setVisibility(View.GONE);
+            mTryAgainButton.setVisibility(View.GONE);
+            EarthquakeAsyncTask earthquakeAsyncTask = new EarthquakeAsyncTask();
+            earthquakeAsyncTask.execute(URL_TO_REQUEST);
+        } else {
+            mInfoText.setText(R.string.verify_connection);
+            mInfoText.setVisibility(View.VISIBLE);
+            mTryAgainButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, ArrayList<Earthquake>> {
+
+        @Override
+        protected void onPreExecute() {
+            mLoadingProgressBar.setVisibility(View.VISIBLE);
+        }
+
         @Override
         protected ArrayList<Earthquake> doInBackground(String... urls) {
             if (urls[0] == null || urls.length < 1) {
@@ -72,6 +117,7 @@ public class EarthquakeActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(ArrayList<Earthquake> earthquakeArrayList) {
+            mLoadingProgressBar.setVisibility(View.GONE);
             // Clear the adapter of previous earthquake data
             mAdapter.clear();
             // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
